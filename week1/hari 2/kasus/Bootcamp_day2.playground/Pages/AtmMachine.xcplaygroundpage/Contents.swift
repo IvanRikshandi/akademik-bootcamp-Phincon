cd7e283b65ceb10248379e109fc7e4a94ae93595
IvanRikshandi <rikshandii@gmail.com>
2023-10-27 17:04:53 +0700

    Table View, Collection, localize string, custom view

---
+++ week1/hari 2/kasus/Bootcamp_day2.playground/Pages/AtmMachine.xcplaygroundpage/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+print("============================")
+print("=========ATM KUBANK=========")
+print("[1] Deposit")
+print("[2] Tarik Tunai")
+print("[3] Cek Saldo")
+print("[4] Exit")
+print("============================")
+
+class ATM{
+    static func saldo = 0
+    if 
+}
